e692720df0ecb9d820004d9cc5bd4121728e71b6
Étienne (Ethan) <lolokiepro@gmail.com>
2025-04-10 19:01:50 -0400

    Changement couleur de fond

--- Frontend/src/Components/layout/Layout.tsx
+++ Frontend/src/Components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 const Layout = () => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-gray-50">
             <Header />
                 <main className="flex-grow container mx-auto px-4 py-6">
                     <Outlet />
